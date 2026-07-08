fa744f9362b7bf5378b86cdcceaf4fd12637221b
Shristi <shristish484@gmail.com>
2026-07-09 00:15:03 +0530

    Time: 0 ms (100%), Space: 43 MB (27.24%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        StringBuilder sb=new StringBuilder("");
+        for(int i=0;i<address.length();i++)
+        {
+            if(Character.isDigit(address.charAt(i)))
+            sb.append(address.charAt(i));
+            else
+            {
+                sb.append("[.]");
+            }
+        }
+        return sb.toString();
+    }
+}
