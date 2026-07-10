e263e73b3d93f5cb60d1a3f148f88cf34fa76956
Shristi <shristish484@gmail.com>
2026-07-10 09:28:18 +0530

    Time: 7 ms (5.08%), Space: 118.9 MB (68.49%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -39,10 +39,10 @@ class Solution {
     }
     public ListNode swapNodes(ListNode head, int k) {
         int length=findlength(head);
-        int a=findval(head,k);
-        int b=findval(head,length-k+1);
-        // ListNode fast=head;
-        // ListNode slow=head;
+        // int a=findval(head,k);
+        // int b=findval(head,length-k+1);
+        //  ListNode fast=head;
+        //  ListNode slow=head;
         // int i=0;
          int idx=0;
         // for(int j=1;j<k;j++)
@@ -60,21 +60,51 @@ class Solution {
         // }
         // System.out.println(i+" "+k);
         // int b=slow.val;
-         ListNode temp=head;
-         System.out.println(a+" "+b);
-        while(temp!=null)
+        //  ListNode temp=head;
+        //  System.out.println(a+" "+b);
+        // while(temp!=null)
+        // {
+        //     idx++;
+        //     if(idx==k)
+        //     {
+        //         temp.val=b;
+        //     }
+        //     else if(idx==length-k+1)
+        //     {
+        //         temp.val=a;
+        //     }
+        //     temp=temp.next;
+        // }
+        // return head;
+
+
+
+
+
+    ListNode fast=head;
+    ListNode slow=head;
+    ListNode el1=null;
+    int a=-1;
+    int b=-1;
+    for(int i=1;i<=k;i++)
+    {
+        if(i==k)
         {
-            idx++;
-            if(idx==k)
-            {
-                temp.val=b;
-            }
-            else if(idx==length-k+1)
-            {
-                temp.val=a;
-            }
-            temp=temp.next;
+            a=fast.val;
+            el1=fast;
         }
-        return head;
+        
+        fast=fast.next;
+    }
+    while(fast!=null)
+    {
+        slow=slow.next;
+        fast=fast.next;
+    }
+    b=slow.val;
+    slow.val=a;
+    el1.val=b;
+    System.out.println(a+" "+b);
+    return head;
     }
 }
