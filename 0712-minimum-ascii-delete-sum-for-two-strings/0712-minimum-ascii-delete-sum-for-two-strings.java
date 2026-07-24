d6bffa6064a43639baae1440a195695540d9f268
Shristi <shristish484@gmail.com>
2026-07-24 18:11:56 +0530

    Time: 15 ms (90.44%), Space: 46.7 MB (27.03%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -3,7 +3,7 @@ class Solution {
         StringBuilder sb=new StringBuilder("");
         int n=x.length();
         int m=y.length();
-        int ans=0;
+        int sum=0;
         int[][] t=new int[n+1][m+1];
         for(int i=0;i<t.length;i++)
         {
@@ -23,46 +23,12 @@ class Solution {
                 t[i][j]=Math.max(t[i-1][j],t[i][j-1]);
             }
         }
-        int i=n;
-        int j=m;
-        
-        while(i>0 && j>0)
-        {
-            if(x.charAt(i-1) == y.charAt(j-1))
-            {
-                
-                i--;
-                j--;
-            }
-            else
-            {
-                if(t[i-1][j]>t[i][j-1])
-                {
-                    ans+=x.charAt(i-1);
-                    System.out.println(x.charAt(i-1));
-                    i--;
-                }
-                
-                else
-                {
-                 ans+=y.charAt(j-1);
-                 System.out.println(y.charAt(j-1));
-                 j--;
-                }
+        for(int i=0;i<n;i++)
+        sum+=x.charAt(i);
+        for(int i=0;i<m;i++)
+        sum+=y.charAt(i);
 
-               
-            }
-        }
-        while(i>0)
-        {   ans+=x.charAt(i-1);
-            i--;
-        }
-        while(j>0)
-        {
-           ans+=y.charAt(j-1);
-            j--;
-        }
 
-        return ans;
+        return sum-2*t[n][m];
     }
 }
