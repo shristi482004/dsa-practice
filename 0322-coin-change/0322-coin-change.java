5ea661aa7f805ba69d7bb3274545fc4eaed1d93a
Shristi <shristish484@gmail.com>
2026-07-27 12:02:34 +0530

    Time: 25 ms (31.27%), Space: 46.8 MB (34.65%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -11,24 +11,22 @@ class Solution {
                 t[i][j]=0;
                 else if(i==1)
                 {
-                    if(j%coins[i-1]==0)
-                    {
-                        t[i][j]=j/coins[i-1];
-                    }
-                    else
-                    t[i][j]=Integer.MAX_VALUE-1;
+                if(j%coins[i-1]!=0)
+                t[i][j]=Integer.MAX_VALUE-1;
+                else 
+                t[i][j]=j/coins[i-1];
                 }
             }
         }
-        for(int i=2;i<t.length;i++)
+        for(int i=1;i<t.length;i++)
         {
             for(int j=1;j<t[0].length;j++)
             {
                 if(coins[i-1]<=j)
-                t[i][j]=Math.min(1+t[i][j-coins[i-1]],t[i-1][j]);
+                t[i][j]=Math.min(t[i-1][j],1+t[i][j-coins[i-1]]);
+                else 
+                t[i][j]=t[i-1][j];
                 
-                else
-                    t[i][j]=t[i-1][j];
                 
             }
         }
