bd67ec0d2c490aadb7ff75ac2bf28b9f2a9796c7
Shristi <shristish484@gmail.com>
2026-07-27 11:11:34 +0530

    Time: 23 ms (40.36%), Space: 53.9 MB (52.58%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,32 +1,24 @@
 class Solution {
     public int longestCommonSubsequence(String x, String y) {
-        StringBuilder sb=new StringBuilder("");
+        int[][] t=new int[x.length()+1][y.length()+1];
         int n=x.length();
         int m=y.length();
-        int[][] t=new int[n+1][m+1];
-        for(int i=0;i<t.length;i++)
-        {
-            for(int j=0;j<t[0].length;j++)
-            {
-                if(i==0 || j==0)
-                t[i][j]=0;
-            }
-        }
         for(int i=1;i<t.length;i++)
         {
             for(int j=1;j<t[0].length;j++)
             {
-                if(x.charAt(i-1) == y.charAt(j-1))
+                if(x.charAt(i-1)==y.charAt(j-1))
                 t[i][j]=1+t[i-1][j-1];
                 else
-                t[i][j]=Math.max(t[i-1][j],t[i][j-1]);
+                 t[i][j]=Math.max(t[i][j-1],t[i-1][j]);
             }
         }
         int i=n;
         int j=m;
+        StringBuilder sb=new StringBuilder("");
         while(i>0 && j>0)
         {
-            if(x.charAt(i-1) == y.charAt(j-1))
+            if(x.charAt(i-1)==y.charAt(j-1))
             {
                 sb.append(x.charAt(i-1));
                 i--;
@@ -34,13 +26,17 @@ class Solution {
             }
             else
             {
-                if(t[i-1][j]>t[i][j-1])
-                i--;
+                if(t[i][j-1]>t[i-1][j])
+                {
+                    j--;
+                }
                 else
-                j--;
+                {
+                    i--;
+                }
             }
         }
-        System.out.println(sb.reverse().toString());
+        
         return t[n][m];
     }
 }
