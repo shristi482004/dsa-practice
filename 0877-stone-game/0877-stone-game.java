bbbb455b2412e27ba3427c4cbdc555d36072da0d
Shristi <shristish484@gmail.com>
2026-07-27 17:48:05 +0530

    Time: 4 ms (24.23%), Space: 43.3 MB (26.1%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int alice=0;
+        int bob=0;
+        Arrays.sort(piles);
+        int j=piles.length-1;
+        int i=0;
+        int turn=1;
+        while(i<j)
+        {
+            if(piles[j]>=piles[i])
+            {
+                if(turn%2==1)
+                alice+=piles[j];
+                else
+                bob+=piles[j];
+                j--;
+            }
+            else
+            {
+                if(turn%2==1)
+                alice+=piles[i];
+                else
+                bob+=piles[i];
+                i--;
+            }
+
+        }
+        
+        return alice>bob;
+    }
+}
