23aa82906a4437c1dfe592d61f7b6d69e6b5a021
Shristi <shristish484@gmail.com>
2026-07-27 12:26:43 +0530

    Time: 15 ms (90.66%), Space: 46.4 MB (72.9%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -1,34 +1,25 @@
 class Solution {
     public int minimumDeleteSum(String x, String y) {
-        StringBuilder sb=new StringBuilder("");
+        int sum=0;
+        
+        int[][] t=new int[x.length()+1][y.length()+1];
         int n=x.length();
         int m=y.length();
-        int sum=0;
-        int[][] t=new int[n+1][m+1];
-        for(int i=0;i<t.length;i++)
-        {
-            for(int j=0;j<t[0].length;j++)
-            {
-                if(i==0 || j==0)
-                t[i][j]=0;
-            }
-        }
-        for(int i=1;i<t.length;i++)
-        {
-            for(int j=1;j<t[0].length;j++)
-            {
-                if(x.charAt(i-1) == y.charAt(j-1))
-                t[i][j]= x.charAt(i-1)+t[i-1][j-1];
-                else
-                t[i][j]=Math.max(t[i-1][j],t[i][j-1]);
-            }
-        }
         for(int i=0;i<n;i++)
         sum+=x.charAt(i);
         for(int i=0;i<m;i++)
         sum+=y.charAt(i);
-
-
+        
+        for(int i=1;i<t.length;i++)
+        {
+            for(int j=1;j<t[0].length;j++)
+            {
+                if(x.charAt(i-1)==y.charAt(j-1))
+                t[i][j]=Math.max(t[i-1][j],x.charAt(i-1)+t[i-1][j-1]);
+                else
+                 t[i][j]=Math.max(t[i][j-1],t[i-1][j]);
+            }
+        }
         return sum-2*t[n][m];
     }
 }
