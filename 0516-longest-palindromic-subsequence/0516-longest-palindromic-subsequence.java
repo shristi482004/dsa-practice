0fe977c198d77762a5697664d190b721fd09f676
Shristi <shristish484@gmail.com>
2026-07-27 11:08:18 +0530

    Time: 59 ms (27.98%), Space: 63.5 MB (77.65%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,28 +1,57 @@
 class Solution {
-    public int longestPalindromeSubseq(String a) {
-        StringBuilder sb=new StringBuilder(a).reverse();
-        String b=sb.toString();
-        int n=a.length();
-        int m=b.length();
-        int[][] t=new int[n+1][m+1];
-        for(int i=0;i<t.length;i++)
-        {
-            for(int j=0;j<t[0].length;j++)
-            {
-                if(i==0 || j==0)
-                t[i][j]=0;
-            }
-        }
+    public int longestPalindromeSubseq(String x) {
+        StringBuilder sb=new StringBuilder(x);
+        sb=sb.reverse();
+        String y=sb.toString();
+        int[][] t=new int[x.length()+1][y.length()+1];
+        int n=x.length();
+        int m=y.length();
         for(int i=1;i<t.length;i++)
         {
             for(int j=1;j<t[0].length;j++)
             {
-                if(a.charAt(i-1)==b.charAt(j-1))
+                if(x.charAt(i-1)==y.charAt(j-1))
                 t[i][j]=1+t[i-1][j-1];
                 else
-                t[i][j]=Math.max(t[i-1][j],t[i][j-1]);
+                 t[i][j]=Math.max(t[i][j-1],t[i-1][j]);
             }
         }
+        // int i=n;
+        // int j=m;
+        // StringBuilder sb=new StringBuilder("");
+        // while(i>0 && j>0)
+        // {
+        //     if(x.charAt(i-1)==y.charAt(j-1))
+        //     {
+        //         sb.append(x.charAt(i-1));
+        //         i--;
+        //         j--;
+        //     }
+        //     else
+        //     {
+        //         if(t[i][j-1]>t[i-1][j])
+        //         {
+        //             sb.append(y.charAt(j-1));
+        //             j--;
+        //         }
+        //         else
+        //         {
+        //             sb.append(x.charAt(i-1));
+        //             i--;
+        //         }
+        //     }
+        // }
+        // while(i>0)
+        // {
+        //    sb.append(x.charAt(i-1));
+        //     i--; 
+        // }
+        // while(j>0)
+        // {
+        //    sb.append(y.charAt(j-1));
+        //     j--;
+        // }
+        // return sb.reverse().toString();
         return t[n][m];
     }
 }
