02d42652dc47f5593589cbc0ab409f98c3f15453
Shristi <shristish484@gmail.com>
2026-07-27 12:17:27 +0530

    Time: 0 ms (100%), Space: 43 MB (23.41%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,25 +1,23 @@
 class Solution {
-    public int houserobber(int[] nums,int init) {
-        if(nums.length==1)
-        return nums[0];
-        int sum=0;
-        for(int i=0;i<nums.length;i++)
-        sum+=nums[i];
-        int[] t=new int[nums.length];
-        t[0]=nums[0];
-        t[1]=Math.max(nums[1],nums[0]);
-        for(int i=2;i<t.length;i++)
+    public int robbing(int[] nums) {
+        if(nums.length<=2)
+        return nums.length==2?Math.max(nums[0],nums[1]):nums[0];
+        int[] t=new int[nums.length+1];
+        t[0]=0;
+        t[1]=nums[0];
+        t[2]=Math.max(nums[0],nums[1]);
+        for(int i=3;i<t.length;i++)
         {
-           t[i]=Math.max(t[i-1],nums[i]+t[i-2]);
-            
+            t[i]=Math.max(t[i-1],nums[i-1]+t[i-2]);
         }
-        return t[nums.length-1];
+        return t[nums.length];
     }
     public int rob(int[] nums) {
-        if(nums.length==1)
-        return nums[0];
-        int ans1=houserobber(Arrays.copyOfRange(nums,0,nums.length-1),0);
-        int ans2=houserobber(Arrays.copyOfRange(nums,1,nums.length),1);
-        return Math.max(ans1,ans2);
+        if(nums.length<=2)
+        return nums.length==2?Math.max(nums[0],nums[1]):nums[0];
+        int part1=robbing(Arrays.copyOfRange(nums,0,nums.length-1));
+        int part2=robbing(Arrays.copyOfRange(nums,1,nums.length));
+
+        return Math.max(part1,part2);
     }
 }
