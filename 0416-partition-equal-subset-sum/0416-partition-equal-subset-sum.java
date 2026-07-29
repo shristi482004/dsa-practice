08932555c5227cf2880c8eba22c13314574783df
Shristi <shristish484@gmail.com>
2026-07-29 10:34:28 +0530

    Time: 55 ms (40.02%), Space: 47.8 MB (43.7%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,45 +1,30 @@
 class Solution {
-    public boolean subsetsum(int[] nums,int sum) {
-        boolean[][] t=new boolean[nums.length+1][sum+1];
-        for(int i=0;i<t.length;i++)
+    public boolean largestsumwt(int[] stones,int sum) {
+   
+        boolean[][] t=new boolean[stones.length+1][sum+1];
+        t[0][0]=true;
+        
+        for(int i=1;i<t.length;i++)
         {
             for(int j=0;j<t[0].length;j++)
             {
-                if(i==0 && j==0)
-                {
-                    t[i][j]=true;
-                }
-                else if(i==0)
-                {
-                    t[i][j]=false;
-                }
-                else if(j==0)
-                t[i][j]=true;
-            }
-        }
-        for(int i=1;i<t.length;i++)
-        {
-            for(int j=1;j<t[0].length;j++)
-            {
-                if(nums[i-1]<=j)
-                {
-                    t[i][j]=t[i-1][j]||t[i-1][j-nums[i-1]];
-                }
+                if(stones[i-1]<=j)
+                t[i][j]=t[i-1][j] || t[i-1][j-stones[i-1]];
                 else
                 t[i][j]=t[i-1][j];
             }
         }
-        return t[nums.length][sum];
+        
+        return t[stones.length][sum];
     }
-    public boolean canPartition(int[] nums) {
+    public boolean canPartition(int[] stones) {
         int sum=0;
-        for(int i=0;i<nums.length;i++)
-        sum+=nums[i];
-
-        if(sum%2!=0)
+        for(int j=0;j<stones.length;j++)
+        sum+=stones[j]; 
+        if((sum)%2!=0)
         return false;
-
-        else
-        return subsetsum(nums,sum/2);
+        return largestsumwt(stones,(sum)/2);
+        
+        
     }
 }
