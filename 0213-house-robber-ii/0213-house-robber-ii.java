d6c69cd3145e9d2ec93a62735cf7da37294bfacc
Shristi <shristish484@gmail.com>
2026-07-30 08:03:56 +0530

    Time: 0 ms (100%), Space: 42.7 MB (78.69%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,23 +1,27 @@
 class Solution {
     public int robbing(int[] nums) {
-        if(nums.length<=2)
-        return nums.length==2?Math.max(nums[0],nums[1]):nums[0];
-        int[] t=new int[nums.length+1];
-        t[0]=0;
-        t[1]=nums[0];
-        t[2]=Math.max(nums[0],nums[1]);
-        for(int i=3;i<t.length;i++)
+        
+        if(nums.length==1)
+        return nums[0];
+        if(nums.length==2)
+        return Math.max(nums[0],nums[1]);
+        int[] dp=new int[nums.length+1];
+        dp[0]=0;
+        dp[1]=nums[0];
+        dp[2]=Math.max(nums[0],nums[1]);
+        for(int i=3;i<=nums.length;i++)
         {
-            t[i]=Math.max(t[i-1],nums[i-1]+t[i-2]);
+            dp[i]=Math.max(dp[i-1],nums[i-1]+dp[i-2]);
         }
-        return t[nums.length];
+        return dp[nums.length];
     }
     public int rob(int[] nums) {
-        if(nums.length<=2)
-        return nums.length==2?Math.max(nums[0],nums[1]):nums[0];
-        int part1=robbing(Arrays.copyOfRange(nums,0,nums.length-1));
-        int part2=robbing(Arrays.copyOfRange(nums,1,nums.length));
-
-        return Math.max(part1,part2);
+        if(nums.length==1)
+        return nums[0];
+        if(nums.length==2)
+        return Math.max(nums[0],nums[1]);
+        int poss1=robbing(Arrays.copyOfRange(nums,0,nums.length-1));
+        int poss2=robbing(Arrays.copyOfRange(nums,1,nums.length));
+        return Math.max(poss1,poss2);
     }
 }
