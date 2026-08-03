60cce86eaf04abc91c441dc8db0c65f379b7dc07
Shristi <shristish484@gmail.com>
2026-08-03 09:50:14 +0530

    Time: 3 ms (52.19%), Space: 43 MB (62.68%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.java
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.java
@@ -1,45 +1,47 @@
 class Solution {
-    static int[][] t=null;
-    public int solve(int[] values, int i, int j) 
-    {
+     static int[][] t=new int[101][101];
+    static int solve(int arr[],int i, int j) {
+        // code here
         if(i>=j)
         return 0;
-
+        
         if(t[i][j]!=-1)
         return t[i][j];
-        int min=Integer.MAX_VALUE;
+    
+        int ans=Integer.MAX_VALUE;
         for(int k=i;k<=j-1;k++)
         {
             int left=0;
             int right=0;
+            
             if(t[i][k]!=-1)
             left=t[i][k];
             else
-            left=solve(values,i,k);
-
+            left=solve(arr,i,k);
+            
             if(t[k+1][j]!=-1)
             right=t[k+1][j];
             else
-            right=solve(values,k+1,j);
-
-            int tempans=left+right+values[i-1]*values[k]*values[j];
-
-            min=Math.min(min,tempans);
-            t[i][j]=min;
+            right=solve(arr,k+1,j);
+            
+            int tempans=right+left+arr[i-1]*arr[k]*arr[j];
+            
+            if(tempans<ans)
+            ans=tempans;
         }
+        t[i][j]=ans;
         
-
         return t[i][j];
+        
     }
-    public int minScoreTriangulation(int[] values) {
-        t=new int[values.length+1][values.length+1];
-        for(int i=0;i<t.length;i++)
+    public int minScoreTriangulation(int[] arr) {
+        for(int i=0;i<101;i++)
         {
-            for(int j=0;j<t[0].length;j++)
+            for(int j=0;j<101;j++)
             {
                 t[i][j]=-1;
             }
         }
-        return solve(values,1, values.length-1);
+        return solve(arr,1,arr.length-1);
     }
 }
