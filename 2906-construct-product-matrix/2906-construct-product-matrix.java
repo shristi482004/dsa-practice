53e514de1c80c1bf94ab17e7e69f5ac617fb000f
Shristi <shristish484@gmail.com>
2026-08-03 09:36:47 +0530

    Time: 12 ms (54.07%), Space: 108.6 MB (27.91%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.java
+++ 2906-construct-product-matrix/2906-construct-product-matrix.java
@@ -1,29 +1,24 @@
 class Solution {
     public int[][] constructProductMatrix(int[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
         int[][] ans=new int[grid.length][grid[0].length];
-    
-
-        long prd=1;
-        for(int i=0;i<n;i++)
+        long pre=1;
+        long suff=1;
+        for(int i=0;i<grid.length;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<grid[0].length;j++)
             {
-                ans[i][j]=(int)(prd%12345);
-                prd=(prd*grid[i][j])%12345;
+               ans[i][j]=(int)(pre%12345);
+               pre=(pre*grid[i][j])%12345;
             }
         }
-        long sprd=1;
-        for(int i=n-1;i>=0;i--)
+        for(int i=grid.length-1;i>=0;i--)
         {
-            for(int j=m-1;j>=0;j--)
+            for(int j=grid[0].length-1;j>=0;j--)
             {
-                ans[i][j]=(ans[i][j]*(int)sprd)%12345;
-                sprd=(sprd*grid[i][j])%12345;
+                ans[i][j]=(int)(suff*ans[i][j])%12345;
+                suff=(suff*grid[i][j])%12345;
             }
         }
-
-       return ans; 
+        return ans;
     }
 }
