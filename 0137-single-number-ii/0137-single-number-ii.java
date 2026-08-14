1dde1044cadde274aefda0cd81213429656d6bdd
Shristi <shristish484@gmail.com>
2026-08-14 08:45:51 +0530

    Time: 3 ms (70.87%), Space: 44.8 MB (97.27%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans=0;
+        for(int bit=0;bit<32;bit++)
+        {
+            int count=0;
+            for(int num:nums)
+            {
+                if((num & (1<<bit))!=0)
+                count++;
+            }
+            if(count%3!=0)
+            ans|=(1<<bit);
+        }
+        return ans;
+    }
+}
