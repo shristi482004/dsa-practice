2fac4c3b21094f47c69dd88c67da9189044d4f55
Shristi <shristish484@gmail.com>
2026-08-21 07:37:04 +0530

    Time: 3 ms (26.1%), Space: 48.9 MB (21.76%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,43 +1,48 @@
 class Solution {
-    public void dfs(int node, boolean[] vis,ArrayList<ArrayList<Integer>> adj)
+    public void dfs(int node,boolean[] vis,ArrayList<ArrayList<Integer>> adj)
     {
         vis[node]=true;
-
-        for(Integer it:adj.get(node))
+        for(int neighbour:adj.get(node))
         {
-            if(!vis[it])
+            if(!vis[neighbour])
             {
-                dfs(it,vis,adj);
+                dfs(neighbour,vis,adj);
             }
         }
     }
-    public int findCircleNum(int[][] isConnected) {
+  
+    public int findCircleNum(int[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+        
+        int count=0;
         ArrayList<ArrayList<Integer>> adj=new ArrayList<ArrayList<Integer>>();
-        for(int i=0;i<isConnected.length;i++)
+        for(int i=0;i<n;i++)
         {
             adj.add(new ArrayList<Integer>());
         }
-        for(int i=0;i<isConnected.length;i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<isConnected.length;j++)
+            for(int j=0;j<m;j++)
             {
-                if(isConnected[i][j]==1 && i!=j)
+                if(grid[i][j]==1 && i!=j)
                 {
                     adj.get(i).add(j);
                     adj.get(j).add(i);
                 }
             }
         }
-        boolean vis[]=new boolean[isConnected.length];
-        int count=0;
-        for(int i=0;i<vis.length;i++)
+        boolean[] vis=new boolean[adj.size()];
+        for(int i=0;i<n;i++)
         {
-            if(!vis[i])
-            {
-                dfs(i,vis,adj);
-                count++;
-            }
+                if(!vis[i])
+                {
+                    count++;
+                    dfs(i,vis,adj);
+                }
         }
         return count;
+        }
+        
     }
-}
+    
