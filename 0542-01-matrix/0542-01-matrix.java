547122a1876cf06b5af120980c6905e9dec99ee2
Shristi <shristish484@gmail.com>
2026-08-22 10:59:01 +0530

    Time: 17 ms (18.93%), Space: 48.6 MB (87.79%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,56 +1,57 @@
-class Pair
-{
+class Pair{
     int first;
     int second;
-    int steps;
-    public Pair(int first,int second,int steps)
+    int dist;
+
+    public Pair(int first,int second,int dist)
     {
         this.first=first;
         this.second=second;
-        this.steps=steps;
+        this.dist=dist;
     }
 }
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
+        Queue<Pair> q=new LinkedList<>();
+        
         int n=mat.length;
         int m=mat[0].length;
-        Queue<Pair> q=new LinkedList<>();
-        int[][] dist=new int[n][m];
         boolean[][] vis=new boolean[n][m];
+        int[][] ans=new int[n][m];
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(mat[i][j]==0)
-                {
-                    q.add(new Pair(i,j,0));
-                    vis[i][j]=true;
+                {q.add(new Pair(i,j,0));
                 }
             }
         }
-        int[] delro={-1,1,0,0};
-        int[] delco={0,0,1,-1};
+
         while(!q.isEmpty())
         {
             int row=q.peek().first;
             int col=q.peek().second;
-            int st=q.peek().steps;
-            q.remove();
-            dist[row][col]=st;
+            int d=q.peek().dist;
+            vis[row][col]=true;
+            ans[row][col]=d;
 
+            q.remove();
+            int[] delro={-1,1,0,0};
+            int[] delco={0,0,-1,1};
             for(int i=0;i<4;i++)
             {
                 int r=row+delro[i];
                 int c=col+delco[i];
 
                 if(r>=0 && r<n && c>=0 && c<m
-                && !vis[r][c])
+                && !vis[r][c] && mat[r][c]==1)
                 {
                     vis[r][c]=true;
-                    q.add(new Pair(r,c,st+1));
+                    q.add(new Pair(r,c,d+1));
                 }
             }
         }
-        return dist;
+        return ans;
     }
 }
