07dec4a5109267efe3e29046181bbb4ec49be990
Shristi <shristish484@gmail.com>
2026-08-24 09:14:27 +0530

    Time: 4 ms (91.36%), Space: 46.9 MB (78.08%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,51 +1,44 @@
 class Solution {
-    public boolean dfscycle(int node,boolean[] vis,boolean[] pathvis, ArrayList<ArrayList<Integer>> adj) {
-        // code here
+    public boolean dfs(int node,boolean[] vis, boolean[] pathvis,ArrayList<ArrayList<Integer>> adj)
+    {
         vis[node]=true;
         pathvis[node]=true;
-        
-        for(int neighbour:adj.get(node))
-        {
-            if(!vis[neighbour])
+        for(int it:adj.get(node))
             {
-                if(dfscycle(neighbour,vis,pathvis,adj))
+                if(!vis[it])
+                {
+                    if(dfs(it,vis,pathvis,adj)==true)
+                    return true;
+                }
+                else if(pathvis[it]==true)
                 return true;
             }
-            else if(pathvis[neighbour])
-            {
-                return true;
-            }
-        }
-        
         pathvis[node]=false;
         
-        return false;
         
+        return false;
     }
-   
     public boolean canFinish(int V, int[][] edges) {
         boolean[] vis=new boolean[V];
         boolean[] pathvis=new boolean[V];
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<ArrayList<Integer>>();
+         ArrayList<ArrayList<Integer>> adj=new ArrayList<ArrayList<Integer>>();
         for(int i=0;i<V;i++)
         {
             adj.add(new ArrayList<Integer>());
-            
         }
         for(int i=0;i<edges.length;i++)
         {
             adj.get(edges[i][0]).add(edges[i][1]);
-            
         }
-        
         for(int i=0;i<V;i++)
         {
             if(!vis[i])
             {
-                if(dfscycle(i,vis,pathvis,adj)==true)
+                if(dfs(i,vis,pathvis,adj)==true)
                 return false;
             }
         }
+        
         return true;
     }
 }
