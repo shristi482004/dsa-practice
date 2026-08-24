54cad92c28550a96147635dd86fc705cddc16407
Shristi <shristish484@gmail.com>
2026-08-24 08:03:57 +0530

    Time: 16 ms (28.29%), Space: 49 MB (59.77%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,57 +1,59 @@
 class Pair{
     int first;
     int second;
-    int dist;
-
-    public Pair(int first,int second,int dist)
+    int time;
+    public Pair(int first,int second,int time)
     {
         this.first=first;
         this.second=second;
-        this.dist=dist;
+        this.time=time;
     }
 }
 class Solution {
-    public int[][] updateMatrix(int[][] mat) {
+    public int[][] updateMatrix(int[][] grid) {
         Queue<Pair> q=new LinkedList<>();
-        
-        int n=mat.length;
-        int m=mat[0].length;
+        int n=grid.length;
+        int m=grid[0].length;
         boolean[][] vis=new boolean[n][m];
-        int[][] ans=new int[n][m];
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(mat[i][j]==0)
-                {q.add(new Pair(i,j,0));
+                if(grid[i][j]==0)
+                {
+                    vis[i][j]=true;
+                    q.add(new Pair(i,j,0));
                 }
             }
         }
-
+        int maxtime=0;
         while(!q.isEmpty())
         {
+            
             int row=q.peek().first;
             int col=q.peek().second;
-            int d=q.peek().dist;
-            vis[row][col]=true;
-            ans[row][col]=d;
-
+            int t=q.peek().time;
+            grid[row][col]=t;
             q.remove();
+
             int[] delro={-1,1,0,0};
-            int[] delco={0,0,-1,1};
+            int[] delco={0,0,1,-1};
+
             for(int i=0;i<4;i++)
             {
-                int r=row+delro[i];
-                int c=col+delco[i];
+            int r=row+delro[i];
+            int c=col+delco[i];
 
-                if(r>=0 && r<n && c>=0 && c<m
-                && !vis[r][c] && mat[r][c]==1)
-                {
-                    vis[r][c]=true;
-                    q.add(new Pair(r,c,d+1));
-                }
+            if(r>=0 && r<n && c>=0 && c<m
+            && !vis[r][c] && grid[r][c]==1)
+            {
+                vis[r][c]=true;
+                q.add(new Pair(r,c,t+1));
             }
+            }
+            
         }
-        return ans;
+       
+        return grid;
     }
 }
